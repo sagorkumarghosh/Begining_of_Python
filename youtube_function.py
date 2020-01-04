3eaf49eb153df88bd348b2fe88f7c417ba926dfb
sagor <sagor.ghosh40@gmail.com>
2020-01-04 15:08:28 -0400

    global variable

--- youtube_function.py
+++ youtube_function.py
@@ -2,7 +2,8 @@
 
 """Function allow us to reuse the code without repeating ourselves. Allow us to put code to specific
 purpose to a single location. So updated in one spot will change the function value we dont need to 
-change it manually in the code line. This is called dry means not using the same code repetedly. 
+change it manually in the code line. This is called dry means not using the same code repetedly.
+The benifit of the function is code reusablity.There is no duplicate code. 
 
 def hello_func():
     print("Hello Function!")
@@ -42,8 +43,7 @@ print(hello_func("Hi", name = 'sagor'))"""
 
 # Example 5:
 
-#y = int(input("Enter a Year"))
-
+"""y = int(input("Enter a Year"))
 month_days = [0, 31, 28, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31]
 def year(y):
     return y % 4 == 0
@@ -60,4 +60,24 @@ def days_in_month(y, month):
 
     return month_days[month]
 
-print(days_in_month(2021, 2))
+print(days_in_month(2021, 2))"""
+
+"""# Example 6:
+
+def calculate(a,b):
+    print("{} {}, the value of a and b".format(a,b))
+    return a+b
+
+print("{}, the result of a and b".format(calculate(10,20)))"""
+
+# Example :7 The need of global variable
+a = 10
+def f1():
+    global a # when we use global keyword the a variable will hold the value of 20 which is declared in locally.
+    a = 20 # we are changing the variable of global variable.
+    print(a)
+def f2():
+    print(a)
+
+f1()
+f2() # result will be both 20 and 20
