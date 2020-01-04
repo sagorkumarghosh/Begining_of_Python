a2f86e3926fa8f67c088abcab7883501c0388651
sagor <sagor.ghosh40@gmail.com>
2020-01-04 15:36:15 -0400

    leap year

--- problem_leap_year.py
+++ problem_leap_year.py
@@ -1,10 +1,18 @@
-a = 10
-def f1():
-    a = 20
-    print(a)
-    
-def f2():
-    print(a)
+y = int(input("Enter a Year"))
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31]
+def year(y):
+    return y % 4 == 0
+    #print("{}, is leap year".format(y))
+    #else:
+     # print("{}, is not leap year".format(y))
 
-f1()
-f2()
+def days_in_month(y, month):
+    #if not 1 <= month <= 12:
+    if month <= 1 or month >=12:
+        return "Invalid Month"
+    if month == 2 and year(y):
+        return 29
+
+    return month_days[month]
+
+print(days_in_month(2021, 2))
